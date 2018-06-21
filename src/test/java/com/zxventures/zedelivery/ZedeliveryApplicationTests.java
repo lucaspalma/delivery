7c87cea51c24ea33463c9ad805dbdf484e29db7c
Lucas Takeshi Rodrigues Palma <lucastakeshipalma@gmail.com>
2018-06-21 14:43:12 -0300

    Criando teste de integração básico de cadastro de um pdv

--- src/test/java/com/zxventures/zedelivery/ZedeliveryApplicationTests.java
+++ src/test/java/com/zxventures/zedelivery/ZedeliveryApplicationTests.java
@@ -1,16 +1,45 @@
 package com.zxventures.zedelivery;
 
+
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ZedeliveryApplicationTests {
 
+	@Autowired
+    private TestRestTemplate restTemplate;
+
 	@Test
-	public void contextLoads() {
+	public void consigoCadastrarUmPdvPassandoTodosOsDadosObrigatorios() {
+		String mutation =
+					"{" +
+					"   \"query\" : \"mutation {" +
+					"      newPdv(" +
+					"         tradingName : \\\"Bar do Bardo\\\"," +
+					"         ownerName : \\\"Bardo\\\"," +
+					"         document : \\\"66.881.980/0001-97\\\"," +
+					"      ) {" +
+					"         tradingName" +
+					"         ownerName" +
+					"         document" +
+					"      }" +
+					"   }\"" +
+					"}";
+		String jsonResposta = "{data={newPdv={tradingName=Bar do Bardo, ownerName=Bardo, document=66.881.980/0001-97}}}";
+
+		ResponseEntity<Object> postForEntity = restTemplate.postForEntity("/graphql", mutation, Object.class);
+		assertEquals(HttpStatus.OK, postForEntity.getStatusCode());
+		assertEquals(jsonResposta, postForEntity.getBody());
 	}
 
 }
