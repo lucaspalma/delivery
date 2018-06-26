57f8f7d8e3416c45dd87a875167cb341fd3c5afc
Lucas Takeshi Rodrigues Palma <lucastakeshipalma@gmail.com>
2018-06-26 17:46:22 -0300

    Considerando as bordas também na hora de buscar uma área de cobertura

--- src/main/java/com/zxventures/zedelivery/repositories/PdvRepository.java
+++ src/main/java/com/zxventures/zedelivery/repositories/PdvRepository.java
@@ -13,7 +13,7 @@ public interface PdvRepository extends CrudRepository<PontoDeVenda, Long> {
 	
 	PontoDeVenda findByDocument(String document);
 
-	@Query("select pdv from PontoDeVenda pdv where within(:point, pdv.coverageArea) = true")
+	@Query("select pdv from PontoDeVenda pdv where within(:point, pdv.coverageArea) = true or touches(:point, pdv.coverageArea) = true")
 	List<PontoDeVenda> searchPdvsThatCovergeThis(@Param("point") Point address);
 	
 }
