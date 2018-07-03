6b51675d4e206459e0898008f421d517b5409db4
Lucas Takeshi Rodrigues Palma <lucastakeshipalma@gmail.com>
2018-07-03 20:37:39 -0300

    Apagando query que não fazia mais sentido que buscava todos os pdvs

--- src/main/java/com/zxventures/zedelivery/resolvers/Query.java
+++ src/main/java/com/zxventures/zedelivery/resolvers/Query.java
@@ -1,15 +1,11 @@
 package com.zxventures.zedelivery.resolvers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKTReader;
 import com.zxventures.zedelivery.exceptions.AddressNotCoveredException;
 import com.zxventures.zedelivery.grapqhql.models.Pdv;
-import com.zxventures.zedelivery.models.PontoDeVenda;
 import com.zxventures.zedelivery.repositories.PdvRepository;
 
 public class Query implements GraphQLQueryResolver {
@@ -20,14 +16,6 @@ public class Query implements GraphQLQueryResolver {
 	public Query(PdvRepository pdvRepository) {
 		this.pdvRepository = pdvRepository;
 	}
-	
-	public Iterable<Pdv> findAllPdvs() {
-		List<Pdv> pdvs = new ArrayList<>();
-		for (PontoDeVenda pontoDeVenda : pdvRepository.findAll()) {
-			pdvs.add(new Pdv(pontoDeVenda));
-		}
-        return pdvs;
-    }
 	
 	public Pdv findPdv(Long id) {
 		return new Pdv(pdvRepository.findById(id).get());
